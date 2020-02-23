d5db2889157e6c0c69d88f2ce2ac1c85ed434966
Khoa Lam <lakhoa1989@gmail.com>
2020-02-23 13:16:15 +0700

    update to Publich version

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # csv-mapper
 CSV Mapper
+
+# Requirements
+2 csv file
+- data.image.csv: 2 column: id, image
+- data.newid.csv: 2 colum: id, id_new
+
+# Result after mapping column id:
+- output_#.csv: 2 column: id_new, image 
--- index.js
+++ index.js
@@ -1,37 +1,81 @@
 const csv = require('csv-parser');
 const fs = require('fs');
-const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+const _ = require('lodash');
 
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+const SAME_COLUMN = '__same__';
+
+const FILE_1 = {
+    filename: 'data.id.csv', // id, id_new
+    data:[]
+}
+
+const FILE_2 = {
+    filename: 'data.info.csv', // id, image, value....
+    requireField : ['image'], // can't be blank
+    data:[]
+}
+const OUTPUT = {
+    filename: "output_",
+    limit: 1000, // write data to file for every 1000 lines
+    header: [ 
+        // ==== EDIT HEADER COLUMN HERE ====
+        { id: 'id', title: 'id' }, 
+        { id: 'image', title: 'image' }
+        // =================================
+    ]
+};
 
 var data = [];
-var data_new = [];
-var count=1;
-fs.createReadStream('dlv.product.info.image.csv').pipe(csv()).on('data', (row) => {
-	if (row.image) {
-		data.push(row);
-	}
-}).on('end', () => {
-    fs.createReadStream('dlv.product.info.id_map.csv').pipe(csv()).on('data', (row) => {
-        if (data_new.length===1000){
-            var csvWriter = createCsvWriter({
-                path: 'out-'+ count++ +'.csv',
-                header: [ { id: 'id', title: 'id' }, { id: 'image', title: 'image' } ]
-            });
-            csvWriter.writeRecords(data_new);
-            data_new = [];
+
+// Check required fields, false if blank
+function notBlank(row, fieldCheck){
+    if (!fieldCheck)
+        return true;
+    // The same column can't be blank
+    if (!row[SAME_COLUMN])
+        return false;
+    // check more required fields array
+    fieldCheck.forEach(field => {        
+        if (!row[field]) {
+            return false;
         }
-        for (let i = 0; i < data.length; i++) {
-            if (data[i].id == row.id) {
-                data_new.push({
-                    "id": row.id_new,
-                    "image": data[i].image
-                });                
-                break;
-            }
-        }
-    })
-    .on('end', () => {        
     });
+    return true;
+}
+
+// Read FILE_1
+fs.createReadStream(FILE_1.filename).pipe(csv()).on('data', (row) => {
+    if (notBlank(row, FILE_1.requireField))
+        FILE_1.data.push(row);
+	
+}).on('end', () => {
+    console.log("Read file 1: "+FILE_1.data.length + " row(s)");
+    // Read FILE_2    
+    fs.createReadStream(FILE_2.filename).pipe(csv()).on('data', (row) => {        
+        if (notBlank(row, FILE_2.requireField))            
+            FILE_2.data.push(row);
+        
+    })
+    .on('end', () => {
+        console.log("Read file 2: "+FILE_2.data.length + " row(s)");
+        // Use _ to join 2 objects with same id
+        let data = _.map(FILE_1.data, function(item){
+            return _.extend(item, _.find(FILE_2.data, { [SAME_COLUMN]: item[SAME_COLUMN] }));
+        });
+        console.log("Mapped file: "+ data.length + " row(s)");
+        // Write data to file
+        let count = 1;
+        while(data.length) {
+            let dataWrite = data.splice(0,OUTPUT.limit);
+            let csvWriter = createCsvWriter({
+                path: OUTPUT.filename+ count++ +'.csv',
+                header: OUTPUT.header
+            });
+            csvWriter.writeRecords(dataWrite);
+        }
+        console.log("Successfully written to new file!");
+    })
 });
 
 
